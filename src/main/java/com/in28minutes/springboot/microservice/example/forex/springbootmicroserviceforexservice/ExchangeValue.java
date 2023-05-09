a525bdea25839b49adb54370c62d259a7e3bb22c
Rashad Chowdhury <78812401+rashad-moves@users.noreply.github.com>
2023-05-09 14:55:37 -0400

    add Repository and Controller

--- src/main/java/com/in28minutes/springboot/microservice/example/forex/springbootmicroserviceforexservice/ExchangeValue.java
+++ src/main/java/com/in28minutes/springboot/microservice/example/forex/springbootmicroserviceforexservice/ExchangeValue.java
@@ -24,4 +24,37 @@ public class ExchangeValue {
 	public ExchangeValue() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+	  super();
+	  this.id = id;
+	  this.from = from;
+	  this.to = to;
+	  this.conversionMultiple = conversionMultiple;
+	
+	}
+	
+	public Long getId() {
+	    return id;
+	  }
+
+	  public String getFrom() {
+	    return from;
+	  }
+
+	  public String getTo() {
+	    return to;
+	  }
+
+	  public BigDecimal getConversionMultiple() {
+	    return conversionMultiple;
+	  }
+	  
+	  public int getPort() {
+	    return port;
+	  }
+
+	  public void setPort(int port) {
+	    this.port = port;
+	  }
 }
